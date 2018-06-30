c9f1606b955c726efa23894052a32a634b89ccf1
Kay Lovelace <kay@makersacademy.com>
2018-06-30 15:28:14 +0100

    Whatever until now

---
+++ Assets/movinabout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movinabout : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f, moveVertical, 0.0f);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/scrollykilly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollykilly : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+	}
+}
